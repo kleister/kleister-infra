ce14ed8ca07cd9fd1a77fe3d06aa9e7d3048e96a
Thomas Boerger <thomas@webhippie.de>
2018-07-15 22:01:54 +0200

    Integrate github management

---
+++ terraform/repositories.tf
@@ -0,0 +1,179 @@
+resource "github_repository" "homebrew-kleister" {
+  name               = "homebrew-kleister"
+  description        = "Kleister: Homebrew"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-android" {
+  name               = "kleister-android"
+  description        = "Kleister: Android client"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-api" {
+  name               = "kleister-api"
+  description        = "Kleister: API server"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-cli" {
+  name               = "kleister-cli"
+  description        = "Kleister: CLI client"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-desktop" {
+  name               = "kleister-desktop"
+  description        = "Kleister: Desktop client"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-docs" {
+  name               = "kleister-docs"
+  description        = "Kleister: Documentation"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-go" {
+  name               = "kleister-go"
+  description        = "Kleister: SDK for Go"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-infra" {
+  name               = "kleister-infra"
+  description        = "Kleister: Infrastructure"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-ios" {
+  name               = "kleister-ios"
+  description        = "Kleister: iOS client"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-js" {
+  name               = "kleister-js"
+  description        = "Kleister: SDK for Javascript"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-php" {
+  name               = "kleister-php"
+  description        = "Kleister: SDK for PHP"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-python" {
+  name               = "kleister-python"
+  description        = "Kleister: SDK for Python"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-ruby" {
+  name               = "kleister-ruby"
+  description        = "Kleister: SDK for Ruby"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-scripts" {
+  name               = "kleister-scripts"
+  description        = "Kleister: Scripting"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
+
+resource "github_repository" "kleister-ui" {
+  name               = "kleister-ui"
+  description        = "Kleister: Web UI"
+  homepage_url       = "https://kleister.tech"
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  has_downloads      = false
+}
